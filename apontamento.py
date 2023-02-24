1c6cb4e2ed701fa56e8517dab5668c92f98cdd68
KaioLuizGuimaraes <126131215+KaioLuizGuimaraes@users.noreply.github.com>
2023-02-24 16:35:14 -0300

    Update apontamento.py

--- apontamento.py
+++ apontamento.py
@@ -46,9 +46,8 @@ if selected == 'Abrir':
             database = 'Base_cl'
             username = 'User\PC-13'
             password = ''
-            # ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
             cnxn = pyodbc.connect(
-                'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';ENCRYPT=no;UID=' + username + ';PWD=' + password)
+                'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
             cursor = cnxn.cursor()
             comando = f"""use Base_cl
                 Insert into info(id, atv, dtini, dtfim, motini, motfim)
