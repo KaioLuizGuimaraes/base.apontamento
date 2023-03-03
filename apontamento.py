89e266d867b8d5a6f1024c0acaead6b6c01bb5a1
KaioLuizGuimaraes <126131215+KaioLuizGuimaraes@users.noreply.github.com>
2023-03-03 16:49:25 -0300

    Update apontamento.py

--- apontamento.py
+++ apontamento.py
@@ -48,7 +48,7 @@ if selected == 'Abrir':
                 # IP ou nome do servidor.
                 'SERVER=192.168.2.52;'
                 # Porta
-                'PORT=8501;'
+                'PORT=49172;'
                 # Banco que será utilizado.
                 'DATABASE=Base_cl;'
                 # Nome de usuário.
