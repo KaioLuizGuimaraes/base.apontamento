1272315a5d3da23a32ddd67fb0d0ff04594eb7b8
KaioLuizGuimaraes <126131215+KaioLuizGuimaraes@users.noreply.github.com>
2023-03-03 16:39:23 -0300

    Update apontamento.py

--- apontamento.py
+++ apontamento.py
@@ -46,7 +46,7 @@ if selected == 'Abrir':
                 # Driver que será utilizado na conexão
                 'DRIVER={ODBC Driver 17 for SQL Server};'
                 # IP ou nome do servidor.
-                'SERVER=192.168.2.52;'
+                'SERVER=177.103.163.165;'
                 # Porta
                 'PORT=1433;'
                 # Banco que será utilizado.
